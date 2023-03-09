3211e20d900c5c70f5c8624271253cb0106e78d4
Caio Felipe <devcaiofelipe15@gmail.com>
2023-03-09 05:57:22 -0300

    feat: add my first custom hook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './useReducerEuseContext/App-useReducerEuseContext';
+import App from './useMyhook/App-useMyhook';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
